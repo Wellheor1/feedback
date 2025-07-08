b3253d7c465bc08a48bfc7614f357501e5108309
well <Well.heor@yandex.ru>
2025-07-08 21:24:59 +0800

    reformat

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ from pydantic import BaseModel, Field
 
 app = FastAPI()
 
+
 class ReviewBodyParam(BaseModel):
     text: str = Field(..., description="Ваш отзыв")
+
 
 class ReviewsQueryParam(BaseModel):
     sentiment: str = Field(..., description="Тип отзыва")
