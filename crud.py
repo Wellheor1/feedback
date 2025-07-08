b66960ed2a27c23def0b86908ac1d6bbad10dc5f
well <Well.heor@yandex.ru>
2025-07-08 21:48:51 +0800

    search

--- crud.py
+++ crud.py
@@ -2,12 +2,20 @@ import datetime
 
 from database import async_session_maker
 from models import Reviews
+from sqlalchemy import (select)
 
 
-async def create(text: str, sentiment: str):
+async def add_review(text: str, sentiment: str):
     async with async_session_maker() as session:
         review = Reviews(text=text, sentiment=sentiment, created_at=datetime.utcnow().isoformat())
         session.add(review)
         await session.commit()
         await session.refresh(review)
         return review
+
+
+async def search_review(sentiment: str):
+    async with async_session_maker() as session:
+        query = select(Reviews).where(Reviews.sentiment == sentiment)
+        result = await session.execute(query)
+        return result.scalars().all()
