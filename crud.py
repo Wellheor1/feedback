30f6bdbe8015fde814d413eef786d7d8875ede27
well <Well.heor@yandex.ru>
2025-07-08 22:35:54 +0800

    comment)

--- crud.py
+++ crud.py
@@ -13,7 +13,8 @@ async def add_review(text_reviews: str, sentiment: str):
         await session.refresh(review)
         return review
 
-
+### Если обязательно sql, то sqlite так себе вариант (нет RETURNING)
+### Если их использовать необходимо в api учесть что возвращатся dict
 async def add_review_sql(text_reviews: str, sentiment: str):
     created_at = datetime.utcnow().isoformat()
     async with async_session_maker() as session:
