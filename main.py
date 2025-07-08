d3cf735e67b46b21fd20ee04b9835031163a9e0e
well <Well.heor@yandex.ru>
2025-07-08 21:17:19 +0800

    api

---
+++ main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI, Body, Query
+from pydantic import BaseModel, Field
+
+app = FastAPI()
+
+class ReviewBodyParam(BaseModel):
+    text: str = Field(..., description="Ваш отзыв")
+
+class ReviewsQueryParam(BaseModel):
+    sentiment: str = Field(..., description="Тип отзыва")
+
+
+@app.post("/reviews", summary="Создание отзыва")
+async def create_reviews(request_body: ReviewBodyParam = Body()):
+    review = request_body.text
+    return {"id": 1, "text": review, "sentiment": "", "created_at": "2025-02-02"}
+
+
+@app.get("/", summary="Получение отзывов по параметрам")
+async def get_reviews(query_params: ReviewsQueryParam = Query()):
+    sentiment = query_params.sentiment
+    return {"ok": True, "message": "Hello World Well"}
