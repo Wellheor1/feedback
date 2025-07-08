06b41b7ed2bf57d53cf4ef246bb40acdcab85fa1
well <Well.heor@yandex.ru>
2025-07-08 21:57:54 +0800

    сохранение

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from fastapi import FastAPI, Body, Query
+
+from crud import add_review
+from models import Reviews
 from schemas import ReviewBodyParam, ReviewsQueryParam
 
 app = FastAPI()
@@ -7,6 +10,14 @@ app = FastAPI()
 @app.post("/reviews", summary="Создание отзыва")
 async def create_reviews(request_body: ReviewBodyParam = Body()):
     review = request_body.text
+    review_lower = review.lower()
+    sentiments = {"хорош": "positive", "люблю": "positive", "плохо": "negative", "ненавиж": "negative"}
+    if review_lower in sentiments:
+        sentiment = sentiments[review_lower]
+    else:
+        sentiment = "neutral"
+    result = await add_review(review, sentiment)
+    print(result)
     return {"id": 1, "text": review, "sentiment": "", "created_at": "2025-02-02"}
 
 
